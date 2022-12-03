21ec4af0071e76a88f5dace3c71d99243aaba1b6
NBaudoux <baudouxn@gmail.com>
2022-12-03 08:06:29 +0100

    Initial commit

---
+++ src/index.tsx
@@ -0,0 +1,45 @@
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+import HeaderButton from "./headerButton";
+import "./index.less";
+
+const Main: React.FC = () => {
+  const title = "Kagerlighed";
+  const tabs = ["Menu", "About me"];
+  let i = -1;
+
+  const content = () => {switch(selectedTab){
+  case 0: {
+    console.log("Test "+selectedTab);
+    return <span>{"Text"}</span>;}
+  case 1: 
+    return <span>{"Text Text"}</span>;
+  case 2: 
+    return <span>{"Text Text Text"}</span>;
+  }};
+
+  const [selectedTab, setSelectedTab] = useState<number>(i+1);
+
+  return (
+    <>
+      <div className="header">
+        <a href="index" className="home-shortcut">{title}</a>
+        {tabs.map((tab) => {
+          i++;
+          return <HeaderButton index={i} key={i} link="" selected={selectedTab} setSelected={setSelectedTab} value={tab} />;
+        })}
+      </div>
+      <div className="content">
+        {content()}
+      </div>
+    </>
+  );
+};
+
+  
+ReactDOM.render(
+  <React.StrictMode>
+    <Main/>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
