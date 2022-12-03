edb5fdb02d8dca622d8eb0a206c7f6ac509cbbb5
NBaudoux <baudouxn@gmail.com>
2022-12-03 16:35:18 +0100

    Contact info

--- src/pages/Menu/index.tsx
+++ src/pages/Menu/index.tsx
@@ -1,13 +1,17 @@
 import React from "react";
 
+import * as config from "./config";
 import "./index.less";
 
 const Menu: React.FC = () => {
   return (
     <div className="menu">
-      <div className="m-page0" />
-      <div className="m-page1" />
-    </div>
+      <div className="m-pages">
+        <div className="m-page0" />
+        <div className="m-page1" />
+      </div>
+      <span>{config.ORDER}</span>
+    </div>  
   );
 };
 
