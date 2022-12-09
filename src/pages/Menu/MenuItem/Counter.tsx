9493c93a1c034d11743171e243438e008f223abf
NBaudoux <baudouxn@gmail.com>
2022-12-09 04:22:17 +0100

    Styling counter

--- src/pages/Menu/MenuItem/Counter.tsx
+++ src/pages/Menu/MenuItem/Counter.tsx
@@ -2,6 +2,8 @@ import { faMinus, faPlus } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
 
+import "./Counter.less";
+
 type CounterProps = {
   number: number,
   setNumber: (x: number) => void
@@ -17,10 +19,10 @@ const Counter: React.FC<CounterProps> = (props) => {
 
   return(
     <div className="counter">
-      <button className="counter-button" onClick={() => updateNumber(-1)}>
+      <button className="counter-button" disabled={number === 0} onClick={() => updateNumber(-1)}>
         <FontAwesomeIcon icon={faMinus} />
       </button>
-      <span>{number}</span>
+      <div className="counter-number">{number}</div>
       <button className="counter-button" onClick={() => updateNumber(1)}>
         <FontAwesomeIcon icon={faPlus} />
       </button>
