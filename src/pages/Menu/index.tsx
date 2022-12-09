9a3aa6944a4d2c6dff5b7aee41c0a149d398ea33
NBaudoux <baudouxn@gmail.com>
2022-12-09 04:09:27 +0100

    Storing basket in session storage

--- src/pages/Menu/index.tsx
+++ src/pages/Menu/index.tsx
@@ -1,16 +1,31 @@
 import React from "react";
+import { useSessionStorage } from "usehooks-ts";
 
 import * as config from "./config";
 import "./images.less";
 import "./index.less";
+import Counter from "./MenuItem/Counter";
 import MenuItem from "./MenuItem/MenuItem";
 
 const Menu: React.FC = () => {
+  const [basket, setBasket] = useSessionStorage(config.BASKET_KEY, config.ITEMS.map(() => 0));
+
+  const updateBasket = (i: number, value: number) => {
+    const newBasket = [...basket];
+    newBasket[i] = value;
+    setBasket(newBasket);
+  };
+
   return (
     <div className="menu">
       <p>{config.ORDER}</p>
       {config.ITEMS.map((item, i) => (
-        <MenuItem key={i} {...item} />
+        <MenuItem key={i} {...item} >
+          <Counter 
+            number={basket[i]}
+            setNumber={(x) => updateBasket(i, x)}
+          />
+        </MenuItem>
       ))}
     </div>  
   );
