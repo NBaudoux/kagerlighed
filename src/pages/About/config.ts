8fa71544922c630b69e06c4f76519b9e500ac70e
NBaudoux <baudouxn@gmail.com>
2022-12-17 20:29:03 +0100

    Age based on date

--- src/pages/About/config.ts
+++ src/pages/About/config.ts
@@ -1,5 +1,11 @@
+import moment from "moment";
+
+const AGE = () => {
+  return moment().diff("1998-06-23", "years", false);
+};
+
 export const CV = [
-  "My name is Eloïse, I'm 24 years old and I'm from Belgium.", 
+  `My name is Eloïse, I'm ${AGE()} years old and I'm from Belgium.`, 
   "I started baking around 3 when I was visiting my grandma in France with whom I was making pies all the time. With time I started baking more and more and it turned into a passion.",
   "At 17, I decided to improve my techniques and learn even more of the pastry world by studying in a pastry school in Brussels.",
   "After 4 years, I finally graduated from pastry, bakery, chocolate making, ice cream making and confectionery.",
