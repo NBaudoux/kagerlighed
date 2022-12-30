e294be9d195423ee42672ae435b5778521595f8a
Nicolas Baudoux <nicolas.baudoux@ulb.be>
2022-12-31 00:22:00 +0100

    Buttons for fullscreen and list

--- src/pages/Menu/index.tsx
+++ src/pages/Menu/index.tsx
@@ -1,20 +1,56 @@
-import React from "react";
+import { faArrowLeft, faArrowRight, faImages, faList } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React, { Fragment, useMemo, useState } from "react";
 
 import * as config from "./config";
 import "./images.less";
 import "./index.less";
-import MenuItem from "./MenuItem";
+import MenuItem, { MenuItemProps } from "./MenuItem";
 
 const Menu: React.FC = () => {
+  const [fullscreenMenu, setFullscreenMenu] = useState(true);
+  const [currentItemIndex, setCurrentItemIndex] = useState<number>(0);
+
+  const updateCurrentItemIndex = (direction: -1 | 1): void => {
+    setCurrentItemIndex((currentItemIndex + direction) % config.ITEMS.length);
+  };
+  
+  const currentItem: MenuItemProps | undefined = useMemo(() => 
+    config.ITEMS.at(currentItemIndex)
+  , [currentItemIndex]);
+
   return (
     <div className="menu">
       <div>
         <p>{config.ORDER}</p>
         <p>{config.DELIVERY}</p>
       </div>
-      {config.ITEMS.map((item, i) => (
-        <MenuItem key={i} {...item} />
-      ))}
+      <div className="m-display">
+        <button onClick={() => setFullscreenMenu(false)}>
+          <FontAwesomeIcon icon={faList} />
+        </button>
+        <button onClick={() => setFullscreenMenu(true)}>
+          <FontAwesomeIcon icon={faImages} />
+        </button>
+      </div>
+      {
+        fullscreenMenu && currentItem != null
+          ? (
+            <Fragment>
+              <MenuItem fullscreen {...currentItem} />
+              <div className="m-arrows">
+                <button onClick={() => updateCurrentItemIndex(-1)}>
+                  <FontAwesomeIcon icon={faArrowLeft} />
+                </button>
+                <button onClick={() => updateCurrentItemIndex(1)}>
+                  <FontAwesomeIcon icon={faArrowRight} />
+                </button>
+              </div>
+            </Fragment>
+          ) : config.ITEMS.map((item, i) => (
+            <MenuItem key={i} {...item} />
+          ))
+      }
     </div>  
   );
 };
