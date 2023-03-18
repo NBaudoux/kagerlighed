78122dc1e69bb76261cbd50af065f401e1fff1aa
Nicolas Baudoux <nicolas.baudoux@ulb.be>
2023-03-18 16:23:54 +0100

    Using datePicker in index

--- src/pages/Menu/OrderSystem/index.tsx
+++ src/pages/Menu/OrderSystem/index.tsx
@@ -1,15 +1,15 @@
-import { faCalendar } from "@fortawesome/free-regular-svg-icons";
 import { faArrowRotateLeft, faCartShopping } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React, { useState } from "react";
-import DatePicker from "react-datepicker";
+
 import "react-datepicker/dist/react-datepicker.css";
 import Popup from "reactjs-popup";
 import { addDays } from "../../../shared/util/dateUtil";
 
-import { ITEMS as MENU_ITEMS } from "../config";
 import * as config from "./config";
 import "./index.less";
+import OrderDatePicker from "./OrderDatePicker";
+import OrderSummary from "./OrderSummary";
 
 type OrderSystemProps = {
   basket: (number | null)[];
@@ -37,29 +37,17 @@ const OrderSystem: React.FC<OrderSystemProps> = (props) => {
           closeOnDocumentClick
           position="top center"
         >
-          {basket?.map((amount, i) => 
-            amount && amount > 0 
-              ? <div key={i}>{`${amount}x ${MENU_ITEMS[i].title}`}</div>
-              : null
-          )}
+          <OrderSummary 
+            basket={basket}
+            deliveryDate={deliveryDate}
+            setDeliveryDate={setDeliveryDate}
+          />
         </Popup>
       </div>
-      <div className="os-calendar">
-        <FontAwesomeIcon 
-          className="os-icon" 
-          icon={faCalendar}
-          title={config.DELIVERY_DATE}
-        />
-        <DatePicker
-          calendarStartDay={1}
-          closeOnScroll
-          excludeDateIntervals={config.EXCLUDED_DAYS}
-          dateFormat="dd MMMM yyyy"
-          onChange={(date) => date ? setDeliveryDate(date) : null}
-          selected={deliveryDate}
-          showWeekNumbers
-        />
-      </div>
+      <OrderDatePicker 
+        deliveryDate={deliveryDate}
+        setDeliveryDate={setDeliveryDate}
+      />
       <FontAwesomeIcon 
         className="os-icon" 
         icon={faArrowRotateLeft} 
